ff5006030a1a8ec3d57f43d4d290db597efc62e8
Nadali Khalili <nadalikh@gmai.com>
2022-06-15 23:23:19 +0430

    begin

--- voicemailSerivce.php
+++ voicemailSerivce.php
@@ -1,7 +1,16 @@
 #!/usr/bin/php
 <?php
+$db = new mysqli("localhost", "root", "expecto-patronum1379", "app");
+if($db->connect_error)
+    die("we cannot connecto to database because: " . $db->connect_error);
+
+function getExtensionDirectoryWhichHasVoicemail(){
+    global $db;
+    $db->query("");
+
+}
 
 while(true){
-        sleep(3);
-echo "hi\n";
+    sleep(3);
+    exec("ls ");
 } 
