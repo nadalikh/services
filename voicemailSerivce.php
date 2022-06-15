e590865a219de0191b0638b80cc6ec0d8a0847c6
centos_nkh <nadalikh@gmail.com>
2022-06-15 19:47:37 +0430

    voip voicemailb service

---
+++ voicemailSerivce.php
@@ -0,0 +1,7 @@
+#!/usr/bin/php
+<?php
+
+while(true){
+        sleep(3);
+echo "hi\n";
+} 
---
+++ voicemailSerivce.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "system entered" 
+php /root/mySweetyBashs/voicemailSerivce.php >>  /var/voicemailServicenkhlogs/serverlog
+while true
+do
+continue
+done
