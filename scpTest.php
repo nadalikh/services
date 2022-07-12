9bd28aa36563cfb811c5d2360f3d6f62c9d7eb7a
Nadali Khalili <nadalikh@gmai.com>
2022-07-12 14:10:07 +0430

    check voicemail output

--- scpTest.php
+++ scpTest.php
@@ -1,6 +1,5 @@
 <?php
 $output = array();
-exec('sshpass -p "expecto-patronum1379" scp -r /var/www/html/mySweetVoices/default/620/INBOX/ root@51.77.106.237:/var/www/html/voipApp/public/voices', $output);
 //exec('echo expecto-patronum1379');
 echo "expecto-patronum1379";
 var_dump($output);
